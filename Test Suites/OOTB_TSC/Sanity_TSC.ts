775a641825e2229056c3605c332ab850a35ce4b9
Gaurav Srivastava <gauravs@pragiti.com>
2020-02-13 21:04:28 +0530

    delete TCS

--- Test Suites/OOTB_TSC/Sanity_TSC.ts
+++ Test Suites/OOTB_TSC/Sanity_TSC.ts
@@ -5,15 +5,5 @@
    <tag></tag>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
-   <testSuiteRunConfigurations>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <runConfigurationId>Chrome</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/OOTB_TS/Sanity_OOTB</testSuiteEntity>
-      </TestSuiteRunConfiguration>
-   </testSuiteRunConfigurations>
+   <testSuiteRunConfigurations/>
 </TestSuiteCollectionEntity>
