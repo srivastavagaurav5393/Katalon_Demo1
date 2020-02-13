b2e9db83e2d0844eba486ef90cc25821dc782dc5
Gaurav Srivastava <gauravs@pragiti.com>
2020-02-13 20:55:48 +0530

    Second Changes

---
+++ Test Suites/OOTB_TS/Smoke_TS.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4115138f-7dd6-4e30-9609-4eaabd5fbbb0</testSuiteGuid>
+   <testCaseLink>
+      <guid>3fc09941-61ab-4dbb-9de7-afc56a17a08f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OOTB_TC/LandingToPage</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
